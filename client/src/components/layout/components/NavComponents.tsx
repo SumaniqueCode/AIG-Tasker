bfefd95bad327a81b93e3b9a309417168242d4b2
Suman Regmi <limitlessplaying@gmail.com>
2026-01-05 21:09:45 +0545

    Minor fix

--- client/src/components/layout/components/NavComponents.tsx
+++ client/src/components/layout/components/NavComponents.tsx
@@ -20,8 +20,8 @@ const NavComponents = () => {
     toast.success("Logged out successfully!")
     navigate('/login');
   };
-  useEffect(()=>{
-    
+  useEffect(() => {
+
   })
   const user = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user") as string) : null;
 
@@ -29,27 +29,29 @@ const NavComponents = () => {
     <>
       <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 1, ml: 'auto' }}>
         {user ? (
-          <Paper onClick={() => setAnchorE2(!anchorE2)} elevation={0} sx={{ display: "flex", alignItems: "center", gap: 2, minWidth: 220, }}  >
-            <Avatar sx={{ background: 'linear-gradient(45deg, #0247e7ff 0%, #002884 90%)', width: 48, height: 48, fontSize: 18, }}>
-              {user.name.charAt(0).toUpperCase()}
-            </Avatar>
-            <Box>
-              <Typography variant="subtitle1" fontWeight={600}>
-                {user.name}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                @{user.username}
-              </Typography>
-            </Box>
-            <Menu sx={{ top: 50 }} open={anchorE2} onClose={() => setAnchorE2(false)} anchorOrigin={{ vertical: "top", horizontal: "right" }} transformOrigin={{ vertical: "top", horizontal: "right" }}
-              PaperProps={{ elevation: 4, sx: { mt: 1, minWidth: 180, borderRadius: 2, overflow: "hidden", }, }} >
-              <MenuItem onClick={handleLogout} sx={{ gap: 1.5, py: 1.2, fontWeight: 500, "&:hover": { bgcolor: "error.light", color: "white", }, }}>
-                <LogoutOutlined fontSize="small" />
-                Logout
-              </MenuItem>
-            </Menu>
-
-          </Paper>
+          <Box sx={{display: 'flex', gap:4, alignItems: 'center'}}>
+            <Link to="/tasks" > <Button variant="contained" size='medium' sx={{ color: 'black', fontWeight: "bold", borderBottom: 2, px: 5, borderRadius: '8px', backgroundColor: 'white' }}>Tasks</Button></Link>
+            <Paper onClick={() => setAnchorE2(!anchorE2)} elevation={0} sx={{ display: "flex", alignItems: "center", gap: 2, minWidth: 220, }}  >
+              <Avatar sx={{ background: 'linear-gradient(45deg, #0247e7ff 0%, #002884 90%)', width: 48, height: 48, fontSize: 18, }}>
+                {user.name.charAt(0).toUpperCase()}
+              </Avatar>
+              <Box>
+                <Typography variant="subtitle1" fontWeight={600}>
+                  {user.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  @{user.username}
+                </Typography>
+              </Box>
+              <Menu sx={{ top: 50 }} open={anchorE2} onClose={() => setAnchorE2(false)} anchorOrigin={{ vertical: "top", horizontal: "right" }} transformOrigin={{ vertical: "top", horizontal: "right" }}
+                PaperProps={{ elevation: 4, sx: { mt: 1, minWidth: 180, borderRadius: 2, overflow: "hidden", }, }} >
+                <MenuItem onClick={handleLogout} sx={{ gap: 1.5, py: 1.2, fontWeight: 500, "&:hover": { bgcolor: "error.light", color: "white", }, }}>
+                  <LogoutOutlined fontSize="small" />
+                  Logout
+                </MenuItem>
+              </Menu>
+            </Paper>
+          </Box>
         ) : (
           <>
             <Link to="/login" >
@@ -68,15 +70,17 @@ const NavComponents = () => {
         <MenuItem sx={{ borderTop: '2px solid' }}><Link to="/tasks" style={{ textDecoration: 'none', color: 'black', paddingInline: '6px', fontWeight: 500 }}> Home</Link></MenuItem>
         {!user ? (
           <>
-            <MenuItem sx={{ borderTop: '2px solid' }}><Link to="/login" style={{ textDecoration: 'none', color: 'black', paddingInline: '6px', fontWeight: 500,  }}>  Login</Link></MenuItem>
+            <MenuItem sx={{ borderTop: '2px solid' }}><Link to="/login" style={{ textDecoration: 'none', color: 'black', paddingInline: '6px', fontWeight: 500, }}>  Login</Link></MenuItem>
             <MenuItem sx={{ borderTop: '2px solid' }}><Link to="/register" style={{ textDecoration: 'none', color: 'black', paddingInline: '6px', fontWeight: 500 }}>Register</Link></MenuItem>
           </>
         ) : (
-
-          <MenuItem onClick={handleLogout} sx={{ gap: 1.5, py: 1.2, fontWeight: 500, "&:hover": { bgcolor: "error.light", color: "white", }, }}>
-            <LogoutOutlined fontSize="small" />
-            Logout
-          </MenuItem>
+          <>
+            <MenuItem sx={{ borderTop: '2px solid' }}><Link to="/tasks" style={{ textDecoration: 'none', color: 'black', paddingInline: '6px', fontWeight: 500, }}>  Tasks</Link></MenuItem>
+            <MenuItem onClick={handleLogout} sx={{ gap: 1.5, py: 1.2, fontWeight: 500, "&:hover": { bgcolor: "error.light", color: "white", }, }}>
+              <LogoutOutlined fontSize="small" />
+              Logout
+            </MenuItem>
+          </>
         )}
       </Menu>
     </>

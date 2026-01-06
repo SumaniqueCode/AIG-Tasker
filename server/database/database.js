958c4e14092aaea77591ca69f0a73d72ce773648
Suman Regmi <limitlessplaying@gmail.com>
2026-01-06 11:38:09 +0545

    Connection caching

--- server/database/database.js
+++ server/database/database.js
@@ -1,13 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+let cached = global.mongoose;
+const uri = "mongodb+srv://aig-tasker:aig-tasker@cluster0.garfpzj.mongodb.net/aig-tasker?retryWrites=true&w=majority";
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 exports.connectDatabase = async () => {
-    try {
-        await mongoose.connect(
-            "mongodb+srv://aig-tasker:aig-tasker@cluster0.garfpzj.mongodb.net/aig-tasker?retryWrites=true&w=majority"
-        );
-        console.log("Connected to MongoDB Successfully!");
-    } catch (error) {
-        console.error("MongoDB connection failed:", error);
-        process.exit(1);
-    }
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(uri, {
+      bufferCommands: false,
+    });
+  }
+
+  cached.conn = await cached.promise;
+  console.log("Connected to MongoDB Successfully!");
+  return cached.conn;
 };
